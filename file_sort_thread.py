774bdaa9be1bcd1756225e99ed373896ff2f3a2d
phenixua <112823718+phenixua@users.noreply.github.com>
2023-10-09 20:35:13 +0300

    2

--- file_sort_thread.py
+++ file_sort_thread.py
@@ -1,32 +1,28 @@
-"""
-Відсортувати файли в папці.
-"""
-
 import argparse
 from pathlib import Path
 from shutil import copyfile
 from threading import Thread
 import logging
 
-"""
---source [-s] 
---output [-o] default folder = dist
-"""
-
+# Створюємо парсер командного рядка:
 parser = argparse.ArgumentParser(description="Sorting folder")
+# Аргумент для вказання вихідної папки:
 parser.add_argument("--source", "-s", help="Source folder", required=True)
+# Аргумент для вказання папки виводу (за замовчуванням - 'dist'):
 parser.add_argument("--output", "-o", help="Output folder", default="dist")
 
-print(parser.parse_args())
+# Отримуємо аргументи командного рядка та зберігаємо їх у словник args:
 args = vars(parser.parse_args())
-print(args)
 
+# Визначаємо шляхи до вихідної та вихідної папок на основі аргументів командного рядка:
 source = Path(args.get("source"))
 output = Path(args.get("output"))
 
+# Створюємо порожній список для збереження списку папок, які потребують обробки:
 folders = []
 
 
+# Функція для рекурсивного обходу та додавання папок у список folders:
 def grabs_folder(path: Path) -> None:
     for el in path.iterdir():
         if el.is_dir():
@@ -34,30 +30,34 @@ def grabs_folder(path: Path) -> None:
             grabs_folder(el)
 
 
+# Функція для копіювання файлів:
 def copy_file(path: Path) -> None:
     for el in path.iterdir():
         if el.is_file():
-            ext = el.suffix[1:]
-            ext_folder = output / ext
+            ext = el.suffix[1:]  # Отримуємо розширення файлу
+            ext_folder = output / ext  # Формуємо шлях до папки з відповідним розширенням
             try:
-                ext_folder.mkdir(exist_ok=True, parents=True)
-                copyfile(el, ext_folder / el.name)
+                ext_folder.mkdir(exist_ok=True, parents=True)  # Створюємо папку з розширенням, якщо її немає
+                copyfile(el, ext_folder / el.name)  # Копіюємо файл у відповідну папку
             except OSError as err:
-                logging.error(err)
+                logging.error(err)  # Логуємо помилку
 
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(threadName)s %(message)s")
+# Встановлюємо рівень логування та формат повідомлень:
+logging.basicConfig(level=logging.INFO, format="%(threadName)s %(message)s")
 
-    folders.append(source)
-    grabs_folder(source)
-    print(folders)
+folders.append(source)  # Додаємо вихідну папку до списку folders
+grabs_folder(source)  # Запускаємо обробку всіх папок
 
-    threads = []
-    for folder in folders:
-        th = Thread(target=copy_file, args=(folder,))
-        th.start()
-        threads.append(th)
+threads = []
 
-    [th.join() for th in threads]
-    print(f"Можна видаляти {source}")
+# Створюємо потоки для обробки кожної папки:
+for folder in folders:
+    th = Thread(target=copy_file, args=(folder,))
+    th.start()
+    threads.append(th)
+
+# Очікуємо завершення роботи всіх потоків:
+[th.join() for th in threads]
+
+print(f"Можна видаляти {source}")  # Виводимо повідомлення про готовність видалення вихідної папки
